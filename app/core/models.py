2452296dbd94a83c9f87d6d66dc7ed2ce7b12039
Constantine Yachnytskyi <yachnytskyi1992@gmail.com>
2022-02-02 08:53:53 +0200

    Update models.py

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,7 @@ import uuid
 import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
+                                       PermissionsMixin
 from django.conf import settings
 
 
